ac8209d40cdb66a7b52481c510033e19cbcb507e
jwkim0341 <92710923+jwkim0341@users.noreply.github.com>
2023-06-24 11:00:20 +0900

    pagination 실행 - screen 반영

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -10,23 +10,61 @@ import 'package:section1/restaurant/provider/restaurant_provider.dart';
 import 'package:section1/restaurant/repository/restaurant_repository.dart';
 import 'package:section1/restaurant/view/restaurant_detail_screen.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener(){
+    // 현재 위치가
+    // 최대 길이보다 조금 덜되는 위치까지 왔다면
+    // 새로운 데이터 추가 요청
+    if(controller.offset > controller.position.maxScrollExtent - 300){
+      ref.read(restaurantProvider.notifier).paginate(
+        fetchMore: true,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
+    // 완전 처음 로딩일 때
     if(data is CursorPaginationLoading){
       return Center(child: CircularProgressIndicator(),);
     }
 
-    // 커서페이지네이션이 들어온 상황(임시적)
+    // 에러
+    if(data is CursorPaginationError){
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    // Cursorpagination
+    // CursorpaginationFetchingMore
+    // CursorpaginationRefetching
+
     final cp = data as CursorPagination;
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: ListView.separated(
+        controller: controller,
         itemCount: cp.data.length,
         itemBuilder: (_, index) {
           final pItem =  cp.data[index];
