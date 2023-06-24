1c2e33c8a262d139f98f82ea72f084b4b0265f38
jwkim0341 <92710923+jwkim0341@users.noreply.github.com>
2023-06-24 13:48:52 +0900

    rating model 작업

---
+++ lib/rating/model/rating_model.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:section1/common/utils/data_utils.dart';
+import 'package:section1/user/model/user_model.dart';
+
+part 'rating_model.g.dart';
+
+@JsonSerializable()
+class RatingModel{
+  final String id;
+  final UserModel user;
+  final int rating;
+  final String content;
+  // 리스트를 집어넣어서 리스트를 반환
+  @JsonKey(
+    fromJson: DataUtils.listPathsToUrls,
+  )
+  final List<String> imgUrl;
+
+  RatingModel({
+  required this.id,
+  required this.user,
+  required this.rating,
+  required this.content,
+  required this.imgUrl,
+ });
+  
+  factory RatingModel.fromJson(Map<String,dynamic>json)
+  => _$RatingModelFromJson(json);
+}
