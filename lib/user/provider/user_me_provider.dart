c0d0373218c6d4ce603ad7307acd0ac0234a650c
jwkim0341 <92710923+jwkim0341@users.noreply.github.com>
2023-07-02 07:12:25 +0900

    user me provider에서 login,logout 함수 작성

--- lib/user/provider/user_me_provider.dart
+++ lib/user/provider/user_me_provider.dart
@@ -2,13 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:section1/common/const/data.dart';
 import 'package:section1/user/model/user_model.dart';
+import 'package:section1/user/repository/auth_repository.dart';
 import 'package:section1/user/repository/user_me_repository.dart';
 
 class UserMeStateNorifier extends StateNotifier<UserModelBase?> {
+  final AuthRepository authRepository;
   final UserMeRepository repository;
   final FlutterSecureStorage storage;
 
   UserMeStateNorifier({
+    required this.authRepository,
     required this.repository,
     required this.storage,
   }) : super(UserModelLoading()) {
@@ -20,7 +23,7 @@ class UserMeStateNorifier extends StateNotifier<UserModelBase?> {
     final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
     final accesshToken = await storage.read(key: ACCESS_TOKEN_KEY);
 
-    if(refreshToken == null || accesshToken == null){
+    if (refreshToken == null || accesshToken == null) {
       state = null;
       return;
     }
@@ -28,5 +31,39 @@ class UserMeStateNorifier extends StateNotifier<UserModelBase?> {
     // 토큰이 존재한다는 가정하에 보낼 수 있음
     final resp = await repository.getMe();
     state = resp;
+  }
+
+  Future<UserModelBase> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      // 로그인을 시도하자 마자 로딩상태로 반환
+      state = UserModelLoading();
+
+      final resp = await authRepository.login(
+        username: username,
+        password: password,
+      );
+
+      await storage.write(key: REFRESH_TOKEN_KEY, value: resp.refreshToken);
+      await storage.write(key: ACCESS_TOKEN_KEY, value: resp.accessToken);
+
+      final userResp = await repository.getMe();
+      state = userResp;
+      return userResp;
+    } catch (error) {
+      state = UserModelError(message: '로그인에 실패했습니다.');
+      return Future.value(state);
+    }
+  }
+
+  Future<void> logout() async {
+    state = null;
+    // 동시에 실행하고 싶을 때
+    await Future.wait([
+      storage.delete(key: REFRESH_TOKEN_KEY),
+      storage.delete(key: ACCESS_TOKEN_KEY),
+    ]);
   }
 }
